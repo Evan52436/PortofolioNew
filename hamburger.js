5df14b4cbbe20dd06707fdddd67f63ff49bac89d
unboiledpotato <evan.pranawaa@gmail.com>
2025-10-18 17:48:11 +0700

    Add files via upload

---
+++ hamburger.js
@@ -0,0 +1,13 @@
+function ham() {
+    const navbar = document.getElementById("navbar");
+    navbar.style.display = 'none';
+}
+
+function show() {
+    const navbar = document.getElementById("navbar");
+    navbar.style.display = 'flex';
+    const ham = document.getElementById("ham")
+    ham.style.display = 'none';
+}
+
+
